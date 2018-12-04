791914fbcf1a2fbc5b9104647a7446286a862f46
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2018-12-04 08:49:33 -0800

    Update 2 test_project.py
    switched to random pull of image to get dx of single image

--- test_project.py
+++ test_project.py
@@ -1,29 +1,31 @@
 import numpy as np # linear algebra
 import pandas as pd #data processing
 
-import os
+import os, random
 from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import cv2
 import csv
 
-''' 
-Read through each x-ray image file name
-'''
 def main():
-    for root, dirs, files in os.walk('/Users/haleymccalpin/Desktop/XRayProject/sample_images'):
-        for img_name in files:
-            print('image:', img_name)
-            get_diagnosis(img_name)
+    #pull random image from file of sample images
+    rand_img = random.choice(os.listdir('/Users/haleymccalpin/Desktop/XRayProject/sample_images'))                       
     
-'''
-Cross-reference x-ray image file name with sample_labels.csv and output image file name with corresponding dx
-'''
-def get_diagnosis(image):
+    #get_diagnosis(rand_img)
+    get_diagnosis(rand_img)
+                             
+def get_diagnosis(image_ID):
+    
+    #open sample_labels.csv and find random image diagnosis
     with open('/Users/haleymccalpin/Desktop/XRayProject/sample_labels.csv') as csvfile:
         sample_labels = csv.reader(csvfile)
+         
         for row in sample_labels: 
-            if row[0] == image:
-                print('image:', image, '--', row[1])
+            if row[0] == image_ID:                
+                #print image ID, visual image, and corresponding diagnosis
+                x_ray = Image.open('/Users/haleymccalpin/Desktop/XRayProject/sample_images/{}'.format(image_ID))
+                print('image ID:', image_ID, '\ndiagnosis:', row[1], '\nimage:', plt.imshow(x_ray))
+
+                
 main()
