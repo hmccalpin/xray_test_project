e33247b1d8c3cb9974b51eb881a784e46324ecc6
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2018-12-07 08:25:42 -0800

    Update xray_project_airflow.py

--- xray_project_airflow.py
+++ xray_project_airflow.py
@@ -31,59 +31,6 @@ def get_dx(**context):
                 return('image ID:', current_rand_img_ID, '\ndiagnosis:', row[1], '\nimage:', plt.imshow(x_ray))
                 return(plt.imshow(x_ray))
 
-        
-def resize():
-    #storing list of image files in dirs variable
-    path = "/Users/haleymccalpin/Desktop/XRayProject/sample_images/"
-    dirs = os.listdir(path)
-    
-    #creates path for resized image files
-    resized_path = path+'resized/'
-    if not os.path.exists(resized_path):
-        os.makedirs(resized_path)
-    
-    #ensures no error message when using PIL Image module, which is only compatible with image file formats
-    if '.DS_Store' in dirs:
-        dirs.remove('.DS_Store')
-    
-    #create counter to keep track of # of images resized
-    resized_counter = 0
-    
-    #loop through all image files in sample_images folder
-    for item in dirs:
-        
-        #ensures only regular files are being opened 
-        if os.path.isfile(path+item):
-            im = Image.open(path+item)
-            
-            #removes .png from original image directory so that we can append 'resized'  
-            og_image_path, ext = os.path.splitext(path+item)
-            
-            #resizes image to 200 pixels (width, height)
-            imResize = im.resize((200,200), Image.ANTIALIAS)
-            
-            #splits image directory into everything leading up to final image ID
-            resized_image_directory, resized_imageID = os.path.split(og_image_path)
-            
-            #saves resized image .png into resized/ folder 
-            imResize.save(resized_path + resized_imageID +' resized.png', 'PNG', quality=90)
-            
-            resized_counter += 1
-            
-    #prints summary statement to dag log
-    logging.info("Resized {} x-ray images".format(resized_counter))
-
-def clear_resized folder():
-    #sets path to resized folder and directory of images in resized folder for future looping purposes
-    resized_path = "/Users/haleymccalpin/Desktop/XRayProject/sample_images/resized/"
-    resized_dirs = os.listdir(path)
-    
-    #loops through all images in resized folder and removes one by one 
-    for item in resized_dirs:
-        os.remove(resized_path+item)
-    
-    #prints summary statement to dag log
-    logging.info("Removed {} x-ray images from resized folder".format(resized_counter))
 
 default_args = {
     'owner': 'me',
@@ -104,18 +51,6 @@ with DAG('xray_project_airflow_v01',
                             python_callable=get_dx,
                             provide_context = True)
 
-with DAG('xray_project_airflow_v02',
-         default_args=default_args,
-         schedule_interval='0 * * * *',         #runs every hour on the hour
-        ) as dag:
-    
-    resize = PythonOperator(task_id='resize',
-                            python_callable=resize)
-    
-    clear_resized_folder = PythonOperator(task_id='clear_resized_folder',
-                                          python_callable=clear_resized_folder)
-    
 
 get_dx >> pull_rand_img
 
-clear_resized_folder >> resize
