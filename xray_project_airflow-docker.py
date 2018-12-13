920ab5d3e2698bb06a6558b4775503dbd69daa1a
hmccalpin <44852403+hmccalpin@users.noreply.github.com>
2018-12-12 16:15:54 -0800

    Update xray_project_airflow-docker.py

--- xray_project_airflow-docker.py
+++ xray_project_airflow-docker.py
@@ -6,9 +6,9 @@ default_args = {
     'owner': 'me',
     'start_date': dt.datetime(2018, 12, 11),
     'retries': 1,
-    'retry_delay': dt.timedelta(minutes=5),
-    'wait_for_downstream': True,
-    'depends_on_past': True
+    'retry_delay': dt.timedelta(minutes=5)
+    #'wait_for_downstream': True,
+    #'depends_on_past': True
 }
 
 with DAG('xray_project_airflow-docker',
@@ -27,7 +27,7 @@ with DAG('xray_project_airflow-docker',
     
     stop_resize_container = BashOperator(task_id='stop_resize_container',
                                          bash_command="docker stop resize",
-                                         trigger_rule='all_success',
+                                         #trigger_rule='all_success',
                                          dag=dag)
                         
 build_resize_container >> run_resize_container >> stop_resize_container
